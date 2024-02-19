36a047a8052c6dbc8745bdd3f15a8e38ba7ab1f9
Guilherme Ciano <dev.personalcontact@proton.me>
2024-02-19 14:06:16 -0300

    feat: Alterações para facilitar o build

--- src/stories/pages/homepage/navbar/index.tsx
+++ src/stories/pages/homepage/navbar/index.tsx
@@ -1,22 +1,25 @@
-import React from "react";
-import { NavbarItems, NavbarWrapper } from "./styles";
-
-import LogoBlack from "../../../assets/Logos/Brand_Black.png";
+import React from 'react';
+import { NavbarItems, NavbarWrapper } from './styles';
 
 const Navbar = () => {
-	return (
-		<NavbarWrapper>
-			<img src={LogoBlack} alt="logo" />
-			<NavbarItems>
-				<a href="#home">Home</a>
-				<a href="#bundle">Bundle Size</a>
-				<a href="#setupInfo">Setup</a>
-				<a href="#installation">Installation</a>
-				<a href="#usage">Usage</a>
-				<a href="#contributing">Contributing</a>
-			</NavbarItems>
-		</NavbarWrapper>
-	);
+  return (
+    <NavbarWrapper>
+      <img
+        src={
+          'https://raw.githubusercontent.com/Guilherme-Ciano/minify-ui/main/src/stories/assets/Logos/Brand_Black.png'
+        }
+        alt="logo"
+      />
+      <NavbarItems>
+        <a href="#home">Home</a>
+        <a href="#bundle">Bundle Size</a>
+        <a href="#setupInfo">Setup</a>
+        <a href="#installation">Installation</a>
+        <a href="#usage">Usage</a>
+        <a href="#contributing">Contributing</a>
+      </NavbarItems>
+    </NavbarWrapper>
+  );
 };
 
 export default Navbar;
