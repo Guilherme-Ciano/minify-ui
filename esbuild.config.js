0c1c98b0c44a0c1651f16e8d6a79e7aa1b071dbd
Guilherme Ciano <dev.personalcontact@proton.me>
2024-02-19 09:13:29 -0300

    Fix

--- esbuild.config.js
+++ esbuild.config.js
@@ -5,9 +5,9 @@ esbuild
     entryPoints: ['./src/index.ts'], // O ponto de entrada da sua biblioteca TypeScript
     bundle: true, // Indica que você deseja criar um único arquivo de saída
     outfile: './dist/bundle.js', // O caminho para o arquivo de saída
-    platform: 'neutral', // Plataforma de destino (node, browser, etc.)
-    format: 'esm', // Formato de saída (CommonJS, ESM, etc.)
+    platform: 'node', // Plataforma de destino (node, browser, etc.)
+    format: 'cjs', // Formato de saída (CommonJS, ESM, etc.)
     sourcemap: true, // Gera sourcemaps para facilitar a depuração
-    target: ['node12'], // Versão do Node.js de destino
+    target: ['node16'], // Versão do Node.js de destino
   })
   .catch(() => process.exit(1));
