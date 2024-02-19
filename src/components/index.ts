023bf9d833089890c42686c787417f7cd15a89ed
Guilherme Ciano <dev.personalcontact@proton.me>
2024-02-19 14:34:17 -0300

    Feat: terminal

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
-export { default as Button } from "./Button";
-export { default as Input } from "./Input";
-export { BasePadding, Column, PageWrapper, Row } from "./Layout";
-export { A, H1, H2, H3, P } from "./Typography";
+export { default as Button } from './Button';
+export { default as Input } from './Input';
+export { BasePadding, Column, PageWrapper, Row } from './Layout';
+export { A, H1, H2, H3, P } from './Typography';
+export { default as Terminal } from './Terminal';
