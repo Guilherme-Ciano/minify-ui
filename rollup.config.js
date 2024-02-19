3a4e0d05dda0b9745683f69c5ec2cef58f1d58b1
Guilherme Ciano <dev.personalcontact@proton.me>
2024-02-19 15:19:34 -0300

    fix: issues on the original files

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,11 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import { dts } from 'rollup-plugin-dts';
 import json from '@rollup/plugin-json';
+import terser from '@rollup/plugin-terser';
 
-const packageJson = require('./package.json');
+import packageJson from './package.json' assert { type: 'json' };
+
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 export default [
   {
@@ -22,10 +25,12 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       json(),
       typescript({ tsconfig: './tsconfig.json', declarationDir: 'dist/types' }),
+      terser(),
     ],
   },
   {
